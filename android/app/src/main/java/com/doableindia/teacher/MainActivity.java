6e65893c80dff28ec93ea86ddb7e57014f72d13b
Deepak <d9717018219@gmail.com>
2026-06-15 16:33:18 +0530

    chore(android): update package name to com.doableindia.teacher

--- android/app/src/main/java/com/doableindia/teacher/MainActivity.java
+++ android/app/src/main/java/com/doableindia/teacher/MainActivity.java
@@ -1,4 +1,4 @@
-package com.doableindia;
+package com.doableindia.teacher;
 
 import android.os.Bundle;
 import android.util.Log;
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.doableindia',
+  appId: 'com.doableindia.teacher',
   appName: 'DoAble India',
   webDir: 'dist',
   server: {
